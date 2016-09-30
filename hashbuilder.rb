0f13894aa759fe237c7eea0497f170e233fd4316
hatlord <rich@Computar.local>
2016-09-30 12:58:18 +0100

    Added other hash types, fixed a bug with table formatting

--- hashbuilder.rb
+++ hashbuilder.rb
@@ -14,18 +14,22 @@ require 'trollop'
 
 
 class Hashes
+
+  attr_reader :hashes, :string
   
   def initialize
-    @strings = []
-    @hashes = []
+    @md5hashes    = []
+    @sha1hashes   = []
+    @sha256hashes = []
+    @sha384hashes = []
+    @sha512hashes = []
   end
 
   def arguments
     @@opts = Trollop::options do 
-      version "hashbuilder 0.1b".light_blue
+      version "hashbuilder 0.1b"
 
       opt :string, "String to hash", :type => String
-      opt :strings, "File containing strings to hash", :type => String
       opt :salt, "Specify a salt to use against all strings", :type => String
 
       if ARGV.empty?
@@ -35,81 +39,101 @@ class Hashes
     end
   end
 
-  def stringarray
-    #build an array of strings to hash here.
-    #This will allow single or multi string input
-    #Do I want to do this? It could get messy really quickly!!
+  def string
+    @@opts[:string]
   end
 
   def header
-    puts "Generating hashes for the string '#{@@opts[:string]}'".red
-    @hashes << ['Hash Type', 'Hash Value']
+    @md5hashes    << ['Hash Type', 'Hash Value']
+    @sha1hashes   << ['Hash Type', 'Hash Value']
+    @sha256hashes << ['Hash Type', 'Hash Value']
+    @sha384hashes << ['Hash Type', 'Hash Value']
+    @sha512hashes << ['Hash Type', 'Hash Value']
   end
 
   def md5
-    @hashes << ["MD5 Hex".green, Digest::MD5.hexdigest(@@opts[:string])]
-    @hashes << ["MD5 with Base64".green, Digest::MD5.base64digest(@@opts[:string])]
+    @md5hashes << ["MD5 Hex", Digest::MD5.hexdigest(@@opts[:string])]
+    @md5hashes << ["MD5 Base64", Digest::MD5.base64digest(@@opts[:string])]
       if @@opts[:salt]
-        @hashes << ["MD5 Hex, prepend salt".light_blue, Digest::MD5.hexdigest(@@opts[:salt] + @@opts[:string])]
-        @hashes << ["MD5 with Base64, prepend salt".light_blue, Digest::MD5.base64digest(@@opts[:salt] + @@opts[:string])]
-        @hashes << ["MD5 Hex, append salt".light_blue, Digest::MD5.hexdigest(@@opts[:string] + @@opts[:salt])]
-        @hashes << ["MD5 with Base64, append salt".light_blue, Digest::MD5.base64digest(@@opts[:string] + @@opts[:salt])]
-    end
-    @hashes
+        @md5hashes << ["MD5 Hex, prepend salt", Digest::MD5.hexdigest(@@opts[:salt] + @@opts[:string])]
+        @md5hashes << ["MD5 Base64, prepend salt", Digest::MD5.base64digest(@@opts[:salt] + @@opts[:string])]
+        @md5hashes << ["MD5 Hex, append salt", Digest::MD5.hexdigest(@@opts[:string] + @@opts[:salt])]
+        @md5hashes << ["MD5 Base64, append salt", Digest::MD5.base64digest(@@opts[:string] + @@opts[:salt])]
+      end
+    @md5hashes
   end
 
   def sha1
-    @hashes << ["SHA1 Hex".green, Digest::SHA1.hexdigest(@@opts[:string])]
-    @hashes << ["SHA1 with Base64".green, Digest::SHA1.base64digest(@@opts[:string])]
+    @sha1hashes << ["SHA1 Hex", Digest::SHA1.hexdigest(@@opts[:string])]
+    @sha1hashes << ["SHA1 Base64", Digest::SHA1.base64digest(@@opts[:string])]
       if @@opts[:salt]
-        @hashes << ["SHA1 Hex, prepend salt".light_blue, Digest::SHA1.hexdigest(@@opts[:salt] + @@opts[:string])]
-        @hashes << ["SHA1 with Base64, prepend salt".light_blue, Digest::SHA1.base64digest(@@opts[:salt] + @@opts[:string])]
-        @hashes << ["SHA1 Hex, append salt".light_blue, Digest::SHA1.hexdigest(@@opts[:string] + @@opts[:salt])]
-        @hashes << ["SHA1 with Base64, append salt".light_blue, Digest::SHA1.base64digest(@@opts[:string] + @@opts[:salt])]
-    end
-    @hashes
+        @sha1hashes << ["SHA1 Hex, prepend salt", Digest::SHA1.hexdigest(@@opts[:salt] + @@opts[:string])]
+        @sha1hashes << ["SHA1 Base64, prepend salt", Digest::SHA1.base64digest(@@opts[:salt] + @@opts[:string])]
+        @sha1hashes << ["SHA1 Hex, append salt", Digest::SHA1.hexdigest(@@opts[:string] + @@opts[:salt])]
+        @sha1hashes << ["SHA1 Base64, append salt", Digest::SHA1.base64digest(@@opts[:string] + @@opts[:salt])]
+      end
   end
 
   def sha256
-    @hashes << ["SHA256 Hex".green, Digest::SHA256.hexdigest(@@opts[:string])]
-    @hashes << ["SHA256 with Base64".green, Digest::SHA256.base64digest(@@opts[:string])]
+    @sha256hashes << ["SHA256 Hex", Digest::SHA256.hexdigest(@@opts[:string])]
+    @sha256hashes << ["SHA256 Base64", Digest::SHA256.base64digest(@@opts[:string])]
       if @@opts[:salt]
-        @hashes << ["SHA256 Hex, prepend salt".light_blue, Digest::SHA256.hexdigest(@@opts[:salt] + @@opts[:string])]
-        @hashes << ["SHA256 with Base64, prepend salt".light_blue, Digest::SHA256.base64digest(@@opts[:salt] + @@opts[:string])]
-        @hashes << ["SHA256 Hex, append salt".light_blue, Digest::SHA256.hexdigest(@@opts[:string] + @@opts[:salt])]
-        @hashes << ["SHA256 with Base64, append salt".light_blue, Digest::SHA256.base64digest(@@opts[:string] + @@opts[:salt])]
+        @sha256hashes << ["SHA256 Hex, prepend salt", Digest::SHA256.hexdigest(@@opts[:salt] + @@opts[:string])]
+        @sha256hashes << ["SHA256 Base64, prepend salt", Digest::SHA256.base64digest(@@opts[:salt] + @@opts[:string])]
+        @sha256hashes << ["SHA256 Hex, append salt", Digest::SHA256.hexdigest(@@opts[:string] + @@opts[:salt])]
+        @sha256hashes << ["SHA256 Base64, append salt", Digest::SHA256.base64digest(@@opts[:string] + @@opts[:salt])]
       end
-    @hashes
   end
 
   def sha384
-    @hashes << ["SHA384 Hex".green, Digest::SHA384.hexdigest(@@opts[:string])]
-    @hashes << ["SHA384 with Base64".green, Digest::SHA384.base64digest(@@opts[:string])]
+    @sha384hashes << ["SHA384 Hex", Digest::SHA384.hexdigest(@@opts[:string])]
+    @sha384hashes << ["SHA384 Base64", Digest::SHA384.base64digest(@@opts[:string])]
+      if @@opts[:salt]
+        @sha384hashes << ["SHA384 Hex, prepend salt", Digest::SHA384.hexdigest(@@opts[:salt] + @@opts[:string])]
+        @sha384hashes << ["SHA384 Base64, prepend salt", Digest::SHA384.base64digest(@@opts[:salt] + @@opts[:string])]
+        @sha384hashes << ["SHA384 Hex, append salt", Digest::SHA384.hexdigest(@@opts[:string] + @@opts[:salt])]
+        @sha384hashes << ["SHA384 Base64, append salt", Digest::SHA384.base64digest(@@opts[:string] + @@opts[:salt])]
+      end
   end
 
   def sha512
-    @hashes << ["SHA512 Hex".green, Digest::SHA512.hexdigest(@@opts[:string])]
-    @hashes << ["SHA512 with Base64".green, Digest::SHA512.base64digest(@@opts[:string])]
+    @sha512hashes << ["SHA512 Hex", Digest::SHA512.hexdigest(@@opts[:string])]
+    @sha512hashes << ["SHA512 Base64", Digest::SHA512.base64digest(@@opts[:string])]
+      if @@opts[:salt]
+        @sha512hashes << ["SHA512 Hex, prepend salt", Digest::SHA512.hexdigest(@@opts[:salt] + @@opts[:string])]
+        @sha512hashes << ["SHA512 Base64, prepend salt", Digest::SHA512.base64digest(@@opts[:salt] + @@opts[:string])]
+        @sha512hashes << ["SHA512 Hex, append salt", Digest::SHA512.hexdigest(@@opts[:string] + @@opts[:salt])]
+        @sha512hashes << ["SHA512 Base64, append salt", Digest::SHA512.base64digest(@@opts[:string] + @@opts[:salt])]
+      end
   end
 
 end
 
 class Printer
-  #output everything here
+
+  def initialize(run)
+    @run = run
+  end
+
+  def printhash
+    puts "\nMD5 hashes for the string '#{@run.string}'".red.bold
+      puts @run.md5.to_table(:first_row_is_head => true)
+    puts "\nSHA1 hashes for the string '#{@run.string}'".red.bold
+      puts @run.sha1.to_table(:first_row_is_head => true)
+    puts "\nSHA256 hashes for the string '#{@run.string}'".red.bold
+      puts @run.sha256.to_table(:first_row_is_head => true)
+    puts "\nSHA384 hashes for the string '#{@run.string}'".red.bold
+      puts @run.sha384.to_table(:first_row_is_head => true)
+    puts "\nSHA512 hashes for the string '#{@run.string}'".red.bold
+      puts @run.sha512.to_table(:first_row_is_head => true)
+  end
+
 end
 
 run = Hashes.new
 run.arguments
 run.header
 
-# puts run.md5.to_table(:first_row_is_head => true)
-puts run.md5.to_table(:first_row_is_head => true)
-puts run.sha1.to_table(:first_row_is_head => true)
-puts run.sha256.to_table(:first_row_is_head => true)
-
-printme = Printer.new
-
-
-###CODE GRAVEYARD####
-    # puts @hashes.to_table(:first_row_is_head => true)
+printme = Printer.new(run)
+printme.printhash
 
