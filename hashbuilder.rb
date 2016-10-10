61fe494f8bbfa79f2c7e50c2f800dc875c742a8b
hatlord <rich@Computar.local>
2016-10-10 16:06:12 +0100

    Indents

--- hashbuilder.rb
+++ hashbuilder.rb
@@ -67,7 +67,7 @@ class Hashes
         @sha1hashes << ["SHA1 Hex, append salt", Digest::SHA1.hexdigest(@@opts[:string] + @@opts[:salt])]
         @sha1hashes << ["SHA1 Base64, append salt", Digest::SHA1.base64digest(@@opts[:string] + @@opts[:salt])]
       end
-      @sha1hashes
+    @sha1hashes
   end
 
   def sha256
@@ -79,7 +79,7 @@ class Hashes
         @sha256hashes << ["SHA256 Hex, append salt", Digest::SHA256.hexdigest(@@opts[:string] + @@opts[:salt])]
         @sha256hashes << ["SHA256 Base64, append salt", Digest::SHA256.base64digest(@@opts[:string] + @@opts[:salt])]
       end
-      @sha256hashes
+    @sha256hashes
   end
 
   def sha384
@@ -91,7 +91,7 @@ class Hashes
         @sha384hashes << ["SHA384 Hex, append salt", Digest::SHA384.hexdigest(@@opts[:string] + @@opts[:salt])]
         @sha384hashes << ["SHA384 Base64, append salt", Digest::SHA384.base64digest(@@opts[:string] + @@opts[:salt])]
       end
-      @sha384hashes
+    @sha384hashes
   end
 
   def sha512
@@ -103,7 +103,7 @@ class Hashes
         @sha512hashes << ["SHA512 Hex, append salt", Digest::SHA512.hexdigest(@@opts[:string] + @@opts[:salt])]
         @sha512hashes << ["SHA512 Base64, append salt", Digest::SHA512.base64digest(@@opts[:string] + @@opts[:salt])]
       end
-      @sha512hashes
+    @sha512hashes
   end
 
 end
